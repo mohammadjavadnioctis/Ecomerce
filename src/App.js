2e09c792606db34e7d8cf7e17fee5836d8bf9191
unknown <mohammadjavadnoctis2@gmail.com>
2020-07-04 11:07:06 -0700

    this is the first push to this repository 2

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import HomePage from './pages/homepage/homepage.component.jsx';
+
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className='App'>
+        <HomePage />
+      </div>
+    );
+  }
+}
+
+export default App;
